e4092abc306e13a2b37b483b227f1f812eacb93c
TYLER BENNETT <tyler.bennett52@gmail.com>
2023-03-08 10:26:09 -0600

    refactor away from multiple envs

--- clients/driver/handler.js
+++ clients/driver/handler.js
@@ -1,8 +1,8 @@
 'use strict';
 
-require('dotenv').config();
+const PORT = require('../../server/halp');
 const { io } = require('socket.io-client');
-const socket = io(`http://localhost:${process.env.PORT}/caps`);
+const socket = io(`http://localhost:${PORT}/caps`);
 
 const handlePickup = (payload) => {
   setTimeout(() => {
---
+++ server/halp.js
@@ -0,0 +1,6 @@
+'use strict';
+
+require('dotenv').config();
+const PORT = process.env.PORT || 3006
+
+module.exports = PORT;
